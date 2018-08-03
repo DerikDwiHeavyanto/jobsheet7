7bfb8ac31d6ecfaa0cb867716d0a1477e563b426
ASUS <ASUS@DESKTOP-RSSJDVP>
2018-08-03 11:13:22 +0700

    coba Nested IF

--- src/jobsheet7/Jobsheet7.java
+++ src/jobsheet7/Jobsheet7.java
@@ -71,6 +71,13 @@ public class Jobsheet7 {
         }else {
             System.out.println("nilai alebih dari 6");
         }
-        
+        System.out.println("\nNested IF");
+        if(a<7){
+            System.out.println("nilai a kurang dari 7");
+            if(a>2);
+            System.out.println("nj=ilai a lebih dari 2");
+            if(a<4);
+            System.out.println("nilai a kurang dari 4");
+        }
     }
 }
